358bf75f5978ecf6a0b818d2153ac89030315e52
davidmano <david_martin_nogueras@hotmail.com>
2024-10-19 19:21:20 +0200

    Update helloworld.ps1 v2

--- helloworld.ps1
+++ helloworld.ps1
@@ -1,13 +1,2 @@
-parallel
-{
-  Write-Output "Hello World - Parallel"
-  Get-Date
-  Start-Sleep -Seconds 3
-  Get-Date
-}
-
-Write-Output "`r`n"
-Write-Output "See you soon again World - Non-Parallel"
-Get-Date
-Start-Sleep -Seconds 3
+Write-Output "Hello World"
 Get-Date
